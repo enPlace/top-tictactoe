aabb5d3d48ee80fe48d19f9c39ab3998bac435bd
enPlace <n.place@protonmail.com>
2021-06-27 16:17:24 -0300

    x/o styling

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,18 @@
+const DOMgameboard = document.getElementById("gameboard")
+const cells = document.querySelectorAll(".cell")
 //gameboard function
+
+cells.forEach(cell=>{
+    
+    if(cell.id%2 ==1){
+        cell.textContent="O"
+        cell.style.color = "#fa46dc"
+    }
+    else{
+        cell.textContent="X"
+        cell.style.color = "#0bd3d3"
+    }
+})
 
 //player object function
 
